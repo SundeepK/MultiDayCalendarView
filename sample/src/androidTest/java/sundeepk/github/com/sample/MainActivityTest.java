23164d5fb563ad6e6660ea8905ab2ee334963d1b
SundeepK <sundeepkahlon@hotmail.com>
2015-10-03 14:33:45 +0100

    Fixed tests to use AndroidJunit runner

--- sample/src/androidTest/java/sundeepk/github/com/sample/MainActivityTest.java
+++ sample/src/androidTest/java/sundeepk/github/com/sample/MainActivityTest.java
@@ -1,6 +1,7 @@
 package sundeepk.github.com.sample;
 
 import android.graphics.Color;
+import android.support.test.InstrumentationRegistry;
 import android.support.test.espresso.ViewAction;
 import android.support.test.espresso.action.CoordinatesProvider;
 import android.support.test.espresso.action.GeneralClickAction;
@@ -9,13 +10,17 @@ import android.support.test.espresso.action.Press;
 import android.support.test.espresso.action.Swipe;
 import android.support.test.espresso.action.Tap;
 import android.support.test.espresso.matcher.ViewMatchers;
+import android.support.test.runner.AndroidJUnit4;
 import android.test.ActivityInstrumentationTestCase2;
+import android.test.suitebuilder.annotation.LargeTest;
 import android.view.View;
 
 import com.github.sundeepk.multidaycalendarview.MultiDayCalendarView;
 import com.github.sundeepk.multidaycalendarview.domain.Event;
 
 import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import java.util.Date;
 
@@ -23,6 +28,8 @@ import static android.support.test.espresso.Espresso.onView;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
+@RunWith(AndroidJUnit4.class)
+@LargeTest
 public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
 
     MultiDayCalendarView.MultiDayCalendarListener multiDayCalendarListener;
@@ -36,6 +43,7 @@ public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActiv
     @Before
     public void setUp() throws Exception {
         super.setUp();
+        injectInstrumentation(InstrumentationRegistry.getInstrumentation());
         activity = getActivity();
         multiDayCalendarListener = mock(MultiDayCalendarView.MultiDayCalendarListener.class);
 
@@ -43,6 +51,7 @@ public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActiv
         multiDayCalendarView.setCalendarListener(multiDayCalendarListener);
     }
 
+    @Test
     public void testItOnNewEventIsCalled(){
         //select first cell in calendar
         ViewAction clickOnCalendar = clickXY(300, 300);
@@ -52,6 +61,7 @@ public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActiv
         verify(multiDayCalendarListener).onNewEventCreate(1443744000L);
     }
 
+    @Test
     public void testItSelectsEvent(){
         final MultiDayCalendarView multiDayCalendarView = (MultiDayCalendarView) activity.findViewById(R.id.multiday_calendar_view);
 
@@ -70,14 +80,26 @@ public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActiv
         verify(multiDayCalendarListener).onEventSelect(1443744000L, new Event<>("Some event", null, Color.parseColor("#43A047")));
     }
 
+    @Test
     public void testItScrollsRight(){
         final MultiDayCalendarView multiDayCalendarView = (MultiDayCalendarView) activity.findViewById(R.id.multiday_calendar_view);
 
         //select first cell in calendar
         onView(ViewMatchers.withId(R.id.multiday_calendar_view)).perform(scroll(100, 100, -600, 0));
 
+        //Mon, 05 Oct 2015 00:00:00 GMT
+        verify(multiDayCalendarListener).onCalendarScroll(new Date(1443830400000L));
+    }
+
+    @Test
+    public void testItScrollsLeft(){
+        final MultiDayCalendarView multiDayCalendarView = (MultiDayCalendarView) activity.findViewById(R.id.multiday_calendar_view);
+
+        //select first cell in calendar
+        onView(ViewMatchers.withId(R.id.multiday_calendar_view)).perform(scroll(100, 100, 600, 0));
+
         //Fri, 02 Oct 2015 00:00:00 GMT
-        verify(multiDayCalendarListener).onCalendarScroll(new Date(1444003200000L));
+        verify(multiDayCalendarListener).onCalendarScroll(new Date(1443571200000L));
     }
 
     public static ViewAction scroll(final int startX, final int startY, final int endX, final int endY){
